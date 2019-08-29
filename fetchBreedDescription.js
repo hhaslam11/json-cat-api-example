564c8866f38d359c9ec0edfa3119a0342f80a482
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-29 21:23:56 +0000

    Wrote unit tests for fetchBreedDescription

--- fetchBreedDescription.js
+++ fetchBreedDescription.js
@@ -3,21 +3,21 @@ const request = require('request');
 const fetchBreedDescription = (breed, cb) => {
   request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (e, response, body) => {
     if (e) {
-      cb(e);
+      cb(e, null);
       return;
     }
     if (response.statusCode !== 200) {
-      cb('Server responded with:' + response.statusCode);
+      cb('Server responded with:' + response.statusCode, null);
       return;
     }
     
     let bodyJson = JSON.parse(body);
     if (!bodyJson.length) {
-      cb('Cat breed not found :(');
+      cb('Cat breed not found :(', null);
       return;
     }
     
-    cb(null, bodyJson[0].description);
+    cb(null, bodyJson[0].description.trim());
   });
 };
 
