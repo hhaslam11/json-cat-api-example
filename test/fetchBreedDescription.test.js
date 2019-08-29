6945b0bf43a2d8423568fa598de1cebe2e2c5c54
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-29 21:07:24 +0000

    Installed mocha and chai

---
+++ test/fetchBreedDescription.test.js
@@ -0,0 +1,6 @@
+const assert = require('chai').assert;
+const fetchBreedDescription = require('../fetchBreedDescription');
+
+describe('#fetchBreedDescription(breed, callback())', () => {
+  it('should return correct information when given a valid breed');
+});
