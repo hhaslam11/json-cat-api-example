f89d27d9387b64b02a6b50d4ad68a0f92571228c
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-29 20:56:17 +0000

    Made code a function

--- index.js
+++ index.js
@@ -5,25 +5,35 @@ const request = require('request');
 const trimError = e => String(e).split('\n')[0];
 
 
+
+
+const fetchBreedDescription = (breed, cb) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + arg, (e, response, body) => {
+    if (e) {
+      cb(e);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      cb('Server responded with:' + response.statusCode);
+      return;
+    }
+    
+    let bodyJson = JSON.parse(body);
+    if (!bodyJson.length) {
+      cb('Cat breed not found :(');
+      return;
+    }
+    
+    cb(null, bodyJson[0].description);
+  });
+};
+
+
 if (!arg) return;
-request('https://api.thecatapi.com/v1/breeds/search?q=' + arg, (e, response, body) => {
+fetchBreedDescription(arg, (e, description) => {
   if (e) {
     console.log(trimError(e));
     return;
   }
-  if (response.statusCode !== 200) {
-    console.log('Server responded with:', response.statusCode);
-    return;
-  }
-
-  let bodyJson = JSON.parse(body);
-  if (!bodyJson.length) {
-    console.log('Cat breed not found :(');
-    return;
-  }
-
-
-  console.log('Breed: ' + bodyJson[0].name);
-  console.log(bodyJson[0].description);
+  console.log(description);
 });
-
